aaa1ff3b1bebaefd9895c57dd7a779c742380246
Tanay <tanayverma98@gmail.com>
2022-08-27 17:51:04 +0530

    PatientRepository imported in suggestionService for findind patients by Id

--- src/main/java/com/hospitalService/hospitalService/Suggestion/SuggestionService.java
+++ src/main/java/com/hospitalService/hospitalService/Suggestion/SuggestionService.java
@@ -1,10 +1,17 @@
 package com.hospitalService.hospitalService.Suggestion;
 
 import com.hospitalService.hospitalService.Doctor.Doctor;
+import com.hospitalService.hospitalService.Patient.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class SuggestionService {
+    private final PatientRepository patientRepository;
+    @Autowired
+    public SuggestionService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
 
     public List<Doctor> suggestDoctors(Long patientId) {
         
